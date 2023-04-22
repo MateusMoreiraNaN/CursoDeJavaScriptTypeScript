d96dcab20019f4a4d2767c0047bba2b2a2f2a97c
“MateusMoreiraNaN <mateusgodoi117@gmail.com>
2023-04-22 14:03:50 -0300

    log

---
+++ Seção10JavaScriptNodeNpmExpress&MongoDB/aula01/app.js
@@ -0,0 +1,4 @@
+const mod = require('./mod1')
+
+console.log(mod.falaNome());
+
---
+++ Seção3JavaScriptLógicaDeProgramação/ObjetoDate.js
@@ -0,0 +1,4 @@
+const tresHoras = 60 * 60 * 3 *1000
+const data = new Date(0 + tresHoras)
+
+console.log(data.toString());
