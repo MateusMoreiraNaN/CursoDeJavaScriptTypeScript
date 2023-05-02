93892884991d6dd13095deca02166f381e87c660
“MateusMoreiraNaN <mateusgodoi117@gmail.com>
2023-05-02 16:08:06 -0300

    save

--- Seção6JavaScriptObjetos&prototypesAvançado/Getters&Setters.js
+++ Seção6JavaScriptObjetos&prototypesAvançado/Getters&Setters.js
@@ -1,14 +1,21 @@
 function Produto(nome, preco, estoque){
-    
+    this.nome = nome
+    this.preco = preco
 
+    let estoquePrivado = estoque
     Object.defineProperty(this, 'estoque',{
         enumerable: true, 
         configurable: false,
         get:() =>{
             return estoque
         },
-        set: ()=>{
-            
+        set: (valor)=>{
+           if(typeof valor !== 'number'){
+                console.log('Bad value');
+                return
+           }
+
+           estoquePrivado = valor
         }
 
     })
@@ -16,9 +23,21 @@ function Produto(nome, preco, estoque){
    
 }
 
+function criaProduto(nome){
+    return{
+        get nome(){
+            return nome
+        },
+        set nome(valor){
+            valo = valor.replace('coisa.', '')
+            nome = valor
+        }
+    }
+}
+
 const p1 = new Produto('Camiseta', 20, 3)
-p1.estoque = 500000000000
+p1.estoque = 'oi mateus'
 delete p1.estoque // configurable
 
-console.log(p1);
+//console.log(p1);
 console.log(p1.estoque);
