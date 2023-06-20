8d23f4f2263f73e959ad1d100614964455cc25d0
MateusMoreiraNaN <mateusgodoi117@gmail.com>
2023-06-20 14:18:37 -0300

    ex01

---
+++ Seção2JavaScriptBásicoParaIniciantes/aula04/aula04.js
@@ -0,0 +1,19 @@
+// isso é um comentário 
+console.log('Olá mundo'); // outro aqui
+
+// aqui
+/*
+console.log('Linha 1');
+console.log('Linha 2');
+console.log('Linha 3');
+console.log('Linha 4');
+console.log('Linha 5');
+*/
+
+console.log('oi');
+
+console.log('Linha 1');
+console.log('Linha 2');
+//console.log('Linha 3');
+console.log('Linha 4');
+console.log('Linha 5');
