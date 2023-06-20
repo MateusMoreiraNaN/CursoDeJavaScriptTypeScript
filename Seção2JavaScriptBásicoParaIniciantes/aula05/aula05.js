1e25dc6fb2dee65d71777dae2832099dcbdf0034
MateusMoreiraNaN <mateusgodoi117@gmail.com>
2023-06-20 14:47:09 -0300

    ex01

---
+++ Seção2JavaScriptBásicoParaIniciantes/aula05/aula05.js
@@ -0,0 +1,14 @@
+let nome = 'Mateus'
+
+
+console.log(`${nome} nasceu em 1984.`);
+console.log('Em 2000', nome ,'conheceu Maria.');
+console.log(nome, 'casou-se com Maria em 2012.');
+nome = 'João'
+console.log('Maria teve 1 filho com', nome ,'em 2015.');
+console.log('O filho de', nome ,'se chama Eduardo.');
+
+// camelCase
+
+let nomeDaMinhaGata = 'ana'
+console.log(nomeDaMinhaGata);
