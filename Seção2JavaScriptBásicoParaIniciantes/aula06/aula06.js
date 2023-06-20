3a6f793c34d3b648bbe0da59b4a4069e2e798b5f
MateusMoreiraNaN <mateusgodoi117@gmail.com>
2023-06-20 15:21:15 -0300

    ex01

---
+++ Seção2JavaScriptBásicoParaIniciantes/aula06/aula06.js
@@ -0,0 +1,12 @@
+const id = 1
+//id = 5
+console.log(id);
+
+const MyName = 'Mateus'
+//MyName = 'Pedro'
+console.log(MyName);
+
+const primeiroNumero = '5'
+const segundoNumero = 10
+const resul = primeiroNumero * segundoNumero
+console.log(typeof (primeiroNumero + segundoNumero));
