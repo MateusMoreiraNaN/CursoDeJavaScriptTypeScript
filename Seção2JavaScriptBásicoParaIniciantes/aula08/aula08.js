097d191805effebe9250059d64ab67c773b9ef51
MateusMoreiraNaN <mateusgodoi117@gmail.com>
2023-06-21 10:12:57 -0300

    save

---
+++ Seção2JavaScriptBásicoParaIniciantes/aula08/aula08.js
@@ -0,0 +1,21 @@
+/*
+const testeVar = (age)=>{
+    var age = 25
+    
+    
+
+    // let só está dispovivel dentro do escopo
+
+}
+*/
+//age = 100
+//console.log(age);
+
+const teste2 = ()=>{
+    let nome = 'Mateus'
+    //return nome
+}
+
+nome = 'F'
+
+console.log(nome);
