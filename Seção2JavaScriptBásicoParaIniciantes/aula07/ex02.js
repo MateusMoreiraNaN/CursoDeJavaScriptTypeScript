0bb9ecc351acbdd77715f8d020b7c74b47a910cd
MateusMoreiraNaN <mateusgodoi117@gmail.com>
2023-06-21 09:56:58 -0300

    save

---
+++ Seção2JavaScriptBásicoParaIniciantes/aula07/ex02.js
@@ -0,0 +1,15 @@
+const nome = 'Mateus Moreira'
+let idade = 35
+let peso = 84
+let altura = 1.80
+let imc = peso / (altura * altura)
+
+console.log(`${nome} tem ${idade} tem anos, pesa ${peso} kg`);
+console.log(`tem ${altura} de altura seu IMC é de ${imc}`);
+
+altura = 2.15
+peso = 102
+imc = peso / (altura * altura)
+
+console.log(' ');
+console.log(`tem ${altura} de altura seu IMC é de ${imc}`);
