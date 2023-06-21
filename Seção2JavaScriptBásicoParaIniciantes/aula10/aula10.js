78ec475b53760f7538f2ac598ecf2d55f8c8967d
MateusMoreiraNaN <mateusgodoi117@gmail.com>
2023-06-21 10:43:22 -0300

    save

---
+++ Seção2JavaScriptBásicoParaIniciantes/aula10/aula10.js
@@ -0,0 +1,39 @@
+/** 
+ * +
+ * -
+ * /
+ * *
+ * **
+ * %
+*/
+
+// NaN
+let a = 'Mateus'
+let b = 5
+console.log(a * b);
+
+let num1 = 5
+let num2 = 10
+console.log(num1 + num2)
+console.log(num1 - num2)
+console.log(num1 / num2)
+console.log(num1 * num2)
+console.log(num1 ** num2)
+console.log(num1 % num2);
+console.log(' ');
+console.log((num2 + num2) * 10);
+
+
+console.log(' ');
+console.log('Contator');
+let contator = 0
+contator++
+contator++
+contator++
+contator++
+contator++
+contator++
+contator++
+contator++
+contator++
+console.log(contator);
