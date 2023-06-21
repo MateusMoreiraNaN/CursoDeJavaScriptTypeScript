78ecec1817e9c34a919f6786f6f3b330d1f4abb9
MateusMoreiraNaN <mateusgodoi117@gmail.com>
2023-06-21 10:23:03 -0300

    save

---
+++ Seção2JavaScriptBásicoParaIniciantes/aula09/aula09.js
@@ -0,0 +1,19 @@
+const nome = 'Luiz'
+const nome1 = "Luiz"
+const nome2 = `Luiz`
+const num1 = 10
+const num2 = 10.80
+let nomeAluno
+const Aluno = null
+const aprovado = false // true ou false
+
+console.log(typeof aprovado);
+
+const a = [1, 2]
+const b = a
+
+console.log(a, b);
+
+b.push(3)
+
+console.log(a,b);
