aa507085ff9d3280d884514a15b39686440ffc57
MateusMoreiraNaN <mateusgodoi117@gmail.com>
2023-06-21 19:45:09 -0300

    num

---
+++ Seção2JavaScriptBásicoParaIniciantes/aula14/aula14.js
@@ -0,0 +1,21 @@
+let num1 = 9.74
+//let num2 = Math.floor(num1) // para baixo
+//let num2 = Math.ceil(num1) // para cima
+let num2 = Math.round(num1)
+
+console.log(num2);
+
+console.log(Math.max(1, 10,54,64,7441154,48489484,1515,5547989,15445123,546494,1854));
+console.log(Math.min(1, 10,54,64,-45,7441154,48489484,1515,5547989,15445123,546494,1854));
+//console.log(Math.random().toFixed(3));
+
+let aleatorio = Math.round(Math.random().toFixed(3) * (100 - 10) + 5)
+
+console.log(aleatorio);
+
+console.log(Math.PI);
+
+console.log(Math.pow(2, 10));
+console.log(2 ** 10);
+
+console.log(100/0);
