ffa43e3709da44c6a711ed134eb0bb79adcab1a9
MateusMoreiraNaN <mateusgodoi117@gmail.com>
2023-06-23 19:03:44 -0300

    save

---
+++ Seção3JavaScript LógicaDeProgramação/aula27/aula27LetConst.js
@@ -0,0 +1,31 @@
+let nome = 'Luiz'
+var nome2 = 'Luiz'
+
+var nome2 = 'otávio'
+
+console.log(nome2);
+
+const verdadeiro = true
+
+if(verdadeiro){
+    let nome = 'otávio'
+    console.log(nome, nome2);
+
+    if(verdadeiro){
+        let nome = 'Outra coisa'
+        console.log('OK');
+        console.log(nome, nome);
+    }
+}
+
+console.log(nome,nome2)
+
+const falaOi = ()=>{
+    let nome = 'João pedro tiago 100 mil'
+    let mySerieFavorite = 'breaking bad'
+    console.log('oi' + nome);
+}
+
+//console.log(mySerieFavorite);
+
+falaOi()
