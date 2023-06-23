23e47b443e2f489c0d50bf2c367fb529140c45f5
MateusMoreiraNaN <mateusgodoi117@gmail.com>
2023-06-23 12:52:34 -0300

    save

---
+++ Seção3JavaScript LógicaDeProgramação/aula19/aula19.js
@@ -0,0 +1,15 @@
+console.log(100 > 99);
+console.log(100 >= 99);
+console.log(100 < 99);
+console.log(100 <= 99);
+console.log(100 == 99);
+console.log(100 === 99);
+console.log(100 != 99);
+console.log(100 !== 99);
+
+console.log(' ');
+
+const a = 10
+const b = '10'
+const comp = a === b
+console.log(comp);
