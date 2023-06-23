d17414798fc2716aa18788732db7ad801d69bbe7
MateusMoreiraNaN <mateusgodoi117@gmail.com>
2023-06-23 17:02:34 -0300

    save

--- Seção3JavaScript LógicaDeProgramação/aula25/aula25.js
+++ Seção3JavaScript LógicaDeProgramação/aula25/aula25.js
@@ -8,3 +8,18 @@ console.log(data.toString());
 const data = new Date(2019, 11)
 console.log(data.toString());
 console.log(data.getDate());
+
+const formData=(data2)=> {
+    const dia = data.getDate()
+    const mes = data.getMonth() + 1
+    const ano = data.getFullYear()
+    const hora = data.getHours()
+    const min = data.getMinutes()
+    const seg = data.getSeconds()
+
+    return `${dia}/${mes}/${ano} ${hora}:${min}:${seg}`
+}
+
+const data2 = new Date()
+const dataBrasil = formData(data2)
+console.log(dataBrasil);
