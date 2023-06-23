2f36431448f2585ffeefbe033bc04840b5f1edb5
MateusMoreiraNaN <mateusgodoi117@gmail.com>
2023-06-23 14:03:35 -0300

    save

---
+++ Seção3JavaScript LógicaDeProgramação/aula20/aula20.js
@@ -0,0 +1,13 @@
+// && and
+// || or
+// ! not
+
+const expressaoAnd = true && true && true && false && true 
+const expressaoOr = true || true || false || true || false || false || true
+
+console.log(expressaoAnd);
+
+console.log(expressaoOr);
+
+console.log(!false);
+
