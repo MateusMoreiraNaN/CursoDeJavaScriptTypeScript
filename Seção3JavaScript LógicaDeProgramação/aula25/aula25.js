73dff58e4ac2e80b6165d3790ce3f70a4288a181
MateusMoreiraNaN <mateusgodoi117@gmail.com>
2023-06-23 16:03:01 -0300

    save

---
+++ Seção3JavaScript LógicaDeProgramação/aula25/aula25.js
@@ -0,0 +1,10 @@
+/*
+const tresHoras = 60 * 60 * 3 * 1000
+const umDia = 60 * 60 * 24 * 1000
+const data = new Date(0 + tresHoras+ umDia)
+console.log(data.toString());
+*/
+
+const data = new Date(2019, 11)
+console.log(data.toString());
+console.log(data.getDate());
