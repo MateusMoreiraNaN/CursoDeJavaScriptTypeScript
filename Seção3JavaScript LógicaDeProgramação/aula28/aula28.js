23e54a84d44a5cfe472de4b6024f8da1ad4f7c95
MateusMoreiraNaN <mateusgodoi117@gmail.com>
2023-06-24 10:53:40 -0300

    save

---
+++ Seção3JavaScript LógicaDeProgramação/aula28/aula28.js
@@ -0,0 +1,14 @@
+const n = [15496, 2254, 3548, 474189, 584846, 66574, 774512, 887451, 901254]
+
+/*
+const primeiroNumero = n[0]
+
+console.log(primeiroNumero);
+*/
+
+const [um, dois, ...resto] = n
+
+console.log(um,dois);
+console.log(resto);
+
+const n2 = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
